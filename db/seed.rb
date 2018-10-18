8d9f78bf105865b2d160c238a7bfce9b039b28fe
magic8baller <lturis@gmail.com>
2018-10-17 22:00:37 -0400

    started

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,16 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require "sqlite3"
+require 'csv'
+
+db = SQLite3::Database.new ":memory:"
+
+rows = db.execute <<-SQL
+
+CREATE TABLE daily_show_guests
+    ID PRIMARY KEY INTEGER,
+    YEAR INTEGER,
+    GoogleKnowlege_Occupation TEXT,
+    Show TEXT,
+    Group TEXT,
+    Raw_Guest_List TEXT;
+
